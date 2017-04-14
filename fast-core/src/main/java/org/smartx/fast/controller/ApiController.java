ee15615b98f77eeb463aa47357cff15ec063a88d
kext <luffyke@gmail.com>
2017-04-14 14:18:37 +0800

    simplify getRemoteIp method

--- fast-core/src/main/java/org/smartx/fast/controller/ApiController.java
+++ fast-core/src/main/java/org/smartx/fast/controller/ApiController.java
@@ -281,19 +281,15 @@ public class ApiController {
      */
     private static String getRemoteIP(HttpServletRequest request) {
 
-        String ip = request.getHeader("X-Real-IP") == null ? null
-                : request.getHeader("X-Real-IP").toString();
+        String ip = request.getHeader("X-Real-IP");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("x-forwarded-for") == null ? null
-                    : request.getHeader("x-forwarded-for").toString();
+            ip = request.getHeader("x-forwarded-for");
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP") == null ? null
-                    : request.getHeader("Proxy-Client-IP").toString();
+            ip = request.getHeader("Proxy-Client-IP");
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP") == null ? null
-                    : request.getHeader("WL-Proxy-Client-IP").toString();
+            ip = request.getHeader("WL-Proxy-Client-IP");
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
