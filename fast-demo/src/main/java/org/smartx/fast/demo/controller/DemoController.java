c44e74fa421df08583a0749f86b5ca7e7b9d8e62
kext <luffyke@gmail.com>
2017-04-15 11:02:49 +0800

    demo controller

--- fast-demo/src/main/java/org/smartx/fast/demo/controller/DemoController.java
+++ fast-demo/src/main/java/org/smartx/fast/demo/controller/DemoController.java
@@ -1,5 +1,8 @@
 package org.smartx.fast.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smartx.fast.annotation.Privilege;
 import org.smartx.fast.bean.ApiRequest;
 import org.smartx.fast.bean.ApiResponse;
 import org.smartx.fast.controller.ApiController;
@@ -17,7 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController extends ApiController {
 
-    public ApiResponse method1(ApiRequest request) {
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    public ApiResponse test(ApiRequest request) {
+        logger.info(key);
+        // get params from request
+        String id = request.getId();
+        logger.info(id);
+        // response value
+        return ApiResponse.ok().addValueToData("name", "kext");
+    }
+
+    @Privilege
+    public ApiResponse testPrivilege(ApiRequest request) {
         return ApiResponse.ok();
     }
 }
